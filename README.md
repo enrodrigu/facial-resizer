76f93a0e915cedcd91f557ca14d4f9a67a01ff42
enrodrigu <erodrigu@enssat.fr>
2023-05-20 22:42:08 +0200

    Added cropping

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# facial-resizer
-A python project that resize images based on face detection
+# Facial Resizer
+A python project that resize images based on face detection, crop the image and resize it to 768x768
+
+## Installation
+
+You need to download opencv trained classifier XML file
+(haarcascade_frontalface_default.xml) [here](https://github.com/opencv/opencv/tree/master/data/haarcascades) and put it in the model folder.
+
+Then copy the images you want to resize in the data folder.
+
+## How to use
+
+Execute the python code with the name of the file to resize as a parameter.
--- face-resizer.py
+++ face-resizer.py
@@ -14,11 +14,40 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ## Detect faces
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-## Draw rectangle around faces
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+ ## Draw rectangle around faces
+'''for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)'''
 
+# --- Cropping ---
+## Get size of image
+height, width, channels = img.shape
+## Cropping image
+if (len(faces) != 0):
+    (f_x, f_y, f_w, f_h) = faces[0]
+else:
+    f_x = 0
+    f_y = 0
+    f_w = width
+    f_h = height
+    print("No face detected")
+
+### Cropping width
+if (f_w*2 > width):
+    margin_w = (width - f_w)//2
+else:
+    margin_w = f_w//2
+
+### Cropping height
+if (f_h*2 > height):
+    margin_h = (height - f_h)//2
+else:
+    margin_h = f_h//2
+
+### Cropping
+img = img[f_y-margin_h:f_y+f_h+margin_h, f_x-margin_w:f_x+f_w+margin_w]
+img = cv2.resize(img, (768, 768))
 
 # --- Output ---
-plt.imshow(img)
-plt.show()
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.imwrite("out\\"+sys.argv[1], img)
